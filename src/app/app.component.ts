38f77ff37b0da10f9d53c913a89c72e5d12ec240
obothlalekganane <elalhtobo@gmail.com>
2020-01-21 04:36:43 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HelloService} from './hello.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hello-world';
+
+  user: any;
+
+  constructor(private helloService: HelloService) {
+
+  }
+
+  ngOnInit() {
+    this.helloService.getAll().subscribe(data => {
+      this.user = data.name + ' ' + data.surname;
+    });
+  }
 }
